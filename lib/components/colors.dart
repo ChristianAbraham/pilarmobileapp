52963be0d40f684bff43238333bceadad6610107
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2024-11-20 21:03:10 +0700

    depedencies and colors

---
+++ lib/components/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color mainPurple = Color.fromRGBO(114, 101, 227, 100);
+const Color accentOrange = Color.fromRGBO(255, 147, 78, 100);
+const Color accentGreen = Color.fromRGBO(65, 204, 124, 100);
+const Color baseWhite = Color.fromRGBO(254, 254, 254, 100);
